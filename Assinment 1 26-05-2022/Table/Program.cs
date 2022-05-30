60837185a0afd187adb162de5dded94bab627b24
Rohit Gupta <rohit.revclerx@gmail.com>
2022-05-30 16:27:26 +0530

    dynamic Array and dynamic mathematic tables

---
+++ Assinment 1 26-05-2022/Table/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Enter First Number");
+string First_Number = Console.ReadLine();
+int First = check(First_Number);
+Console.WriteLine("Enter Second Number");
+string Second_Number = Console.ReadLine();
+int Second = check(Second_Number);
+
+
+
+int check(string Input)
+
+{
+    string p = Input;
+    int j;
+    while (!int.TryParse(p, out j))
+    {
+        Console.WriteLine("please enter again");
+        p = Console.ReadLine();
+    }
+    return j;
+
+}
+
+for(int i = First; i <= Second; i++)
+{
+    for (int j = 1; j <=10; j++)
+    {
+        Console.WriteLine($"{i} X {j} = {i * j}");
+    }
+    Console.WriteLine("-------------------------------------");
+}
---
+++ Assinment 1 26-05-2022/matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace matrix
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value for ROWS  ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for Cols  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[p,n];
+           
+
+            //printing values of array using for each loop
+            for (int i = 0; i < p; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Enter ROW {i} Enter Column {j} value ");
+                    arr[i,j]= Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i <p; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{arr[i,j]}  ");
+                    //arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            //printing the values of array using nested for loop
+            Console.ReadKey();
+        }
+    }
+}
