af241d80f18814dc8f92618f74b1099066d1ef5a
Rohit Gupta <rohit.revclerx@gmail.com>
2022-06-08 18:06:22 +0530

    new polymorphism project start

---
+++ Assinment 1 26-05-2022/polymorphism/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace Polymorphism
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            string a = Console.ReadLine();
+            double rj;
+            Usd r = new Usd();
+            Data = 12;
+            r.convert();
+        }
+    }
+}
+abstract class convertCurrency
+{
+    public abstract double convert();
+
+}
+class Usd : convertCurrency
+{
+    private double input;
+    public double Data  // property
+
+    {
+        get
+        {
+            return this.input;
+        }
+        set
+        {
+            this.input = value;
+        }
+
+    }
+    public override double convert()
+    {
+        return input * 76;
+    }
+
+}
+class inr : convertCurrency
+{
+    private double input;
+    public double data  
+    {
+     
+        get
+        {
+            return this.input;
+        }
+        set
+        {
+            this.input = value;
+        } 
+    }
+    public override double convert()
+    {
+        return input * 1.00;
+    }
+
+}
+class pound : convertCurrency
+{
+    private double input;
+    public double data 
+    {
+         get
+        {
+            return this.input;
+        }
+        set
+        {
+            this.input = value;
+        }
+    }
+    public override double convert()
+    {
+        return input * 97.42;
+    }
+}
