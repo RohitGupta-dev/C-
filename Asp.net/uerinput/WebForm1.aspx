ea44dbd5af82e19d6590294efa696d822f5b611c
Rohit Gupta <rohit.revclerx@gmail.com>
2022-07-15 18:21:32 +0530

    UserData Insert into Db Using Asp.net 2nd Project Addden

---
+++ Asp.net/uerinput/WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="uerinput.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" ID="Lname">Name</asp:Label>
+            <asp:TextBox runat="server" ID="name"></asp:TextBox>
+              <asp:Button ID="Submit" runat="server" OnClick="Submit_Click" text="Submit" />  
+        </div>
+    </form>
+</body>
+</html>
---
+++ Asp.net/uerinput/connect.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace uerinput
+{
+    public class connect
+    {
+        SqlConnection con = new SqlConnection();
+        SqlDataAdapter Adp = new SqlDataAdapter();
+        SqlCommand cmd = new SqlCommand();
+        DataSet data;
+
+
+        public void open_conection()
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Open();
+            }
+            string str = System.Configuration.ConfigurationManager.ConnectionStrings[""].ConnectionString;
+            con.ConnectionString = str;
+            con.Open();
+        }
+        public void close_connection()
+        {
+            con.Close();
+        }
+        public DataSet queryExecuter(string DATA)
+        {
+            try
+            {
+                data = new DataSet();
+                open_conection();
+                cmd.Connection = con;
+                cmd.CommandText = DATA;
+                cmd.CommandTimeout = 2000000;
+                Adp.SelectCommand = cmd;
+                Adp.Fill(data);
+                close_connection();
+                return data;
+            }
+            catch
+            {
+                close_connection();
+                return data;
+            }
+        }
+    }
+}
